4d69607047a51c22511ff7a2aedfebaf55eaa07e
MiltonQuirino <miltonquirino.br@gmail.com>
2022-03-22 07:40:23 -0300

    add success widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:gasolina_alcool/widgets/input.widget.dart';
 import 'package:gasolina_alcool/widgets/loading-button.widget.dart';
 import 'package:gasolina_alcool/widgets/logo.widget.dart';
+import 'package:gasolina_alcool/widgets/success.widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,37 +39,9 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           const Logo(),
-          Container(
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  "Compensa utilizar X",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: "Big Shoulders Display",
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                LoadingButton(
-                  busy: false,
-                  func: () {},
-                  invert: true,
-                  text: "Calcular novamente",
-                )
-              ],
-            ),
+          Success(
+            reset: () {},
+            result: "Compensa X",
           ),
           Input(
             label: "Gasolina",
